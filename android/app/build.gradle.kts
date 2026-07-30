248c0285c38af56be889805b7e5aa1538fc503b8
Prakhar Doneria <gamesprotec@gmail.com>
2026-07-30 22:11:06 +0530

    Google Play Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
     defaultConfig {
         applicationId = "in.softbridgelabs.text"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
